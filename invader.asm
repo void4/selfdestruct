a7e6f6dc707dc179fe1f7133d057708aead4f162
void4 <void4@users.noreply.github.com>
2017-03-29 19:04:55 +0200

    Bounce!

--- invader.asm
+++ invader.asm
@@ -5,13 +5,26 @@ set one 1
 set p1 3041
 set p1x 32
 set p1y 32
-set p1dx 1
-set p1dy 1
-
+set dirx 0
+set diry 0
+set walll 2
+set wallr 62
 loop:
 
 ;move particle
+jumpe dirx one eright
+addi p1x 1
+jump eleftright
+eright:
+subi p1x 1
+eleftright:
+
+jumpe diry one eup
 addi p1y 1
+jump eupdown
+eup:
+subi p1y 1
+eupdown:
 
 ;draw particle
 seti p1 0
@@ -36,6 +49,26 @@ subi mouse 2
 seti mouse 255
 addi mouse 1
 mousenotmoved:
+
+;check if particle is at mouse position
+jumpn mouse p1 unequal
+set diry 1
+jump endif
+unequal:
+jumpg p1y one skip
+set diry 0
+skip:
+endif:
+
+;check for wall collision
+jumpg p1x walll skipleft
+set dirx 1
+skipleft:
+
+jumpg wallr p1x skipright
+set dirx 0
+skipright:
+
 
 
 jump loop
